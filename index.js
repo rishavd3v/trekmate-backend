490cf026ccb47c8b424ef74a1af84a46329fc162
Rishav <rishavraj4273@gmail.com>
2025-07-10 23:39:16 +0530

    fix: cors origin

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ require('dotenv').config();
 const cors = require('cors');
 const app = express();
 const trekRoute = require('./routes/trekRoute');
-const verifyOrigin = require('./middleware/origin');
 
 const port  = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors({
+    origin: "https://trekmate-delta.vercel.app",
+}));
+
 app.use(express.json());
 app.use('/api',trekRoute)
 
